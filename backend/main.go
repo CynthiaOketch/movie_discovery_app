931860adfafd659d13da0a50d09928c9f67be534
coketch <cynthiaoketch6@gmail.com>
2025-06-24 11:15:21 +0300

    set up file structure

---
+++ backend/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	http.HandleFunc("/api/search", SearchHandler)
+	http.HandleFunc("/api/details", DetailHandler)
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
